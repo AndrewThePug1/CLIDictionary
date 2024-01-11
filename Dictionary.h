d0157ffc63c833ee2f89c8a4454c54d6d5ef7538
AndrewThePug1 <andrewmondejarp@gmail.com>
2024-01-11 12:24:17 -0600

    Dictionary.h method operations for step 4

--- Dictionary.h
+++ Dictionary.h
@@ -1,15 +1,29 @@
-#ifndef DICTIONARY_H
-#define DICTIONARY_H
-
-#include <string>
 #include <unordered_map>
+#include <string>
 
 class Dictionary {
 public:
-    Dictionary() = default;
+    void addEntry(const std::string& word, const std::string& definition) {
+        dictionary[word] = definition;
+    }
+
+    std::string searchEntry(const std::string& word) {
+        auto it = dictionary.find(word);
+        if (it != dictionary.end()) {
+            return it->second;
+        } else {
+            return "Word not found";
+        }
+    }
+
+    void updateEntry(const std::string& word, const std::string& newDefinition) {
+        dictionary[word] = newDefinition;
+    }
+
+    void deleteEntry(const std::string& word) {
+        dictionary.erase(word);
+    }
 
 private:
-    std::unordered_map<std::string, std::string> dictionary_;
+    std::unordered_map<std::string, std::string> dictionary;
 };
-
-#endif // DICTIONARY_H
